e0805d9e599467a5d864ac9e69e96a278cdfe1f5
volrath <gerrard_@hotmail.com>
2013-05-24 21:57:41 +0200

    commit final

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Volrath
+ */
+public class helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
